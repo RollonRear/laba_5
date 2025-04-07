5645c276c966fba67b95f09a3699bcd664e92c0c
Дмитрий <mitya_drozdov@mail.ru>
2025-04-07 15:03:04 +0300

    Delete src directory

--- src/laba5/Client.java
+++
@@ -1,8 +0,0 @@
-package laba5;
-
-public abstract class Client {
-	abstract public void request(Executor execute, String massage);
-	
-	abstract public void send(Executor execute);
-	abstract public String sendArg();
-}
--- src/laba5/Color.java
+++
@@ -1,9 +0,0 @@
-package laba5;
-
-public enum Color {
-    GREEN,
-    RED,
-    BLUE,
-    ORANGE,
-    WHITE;
-}
